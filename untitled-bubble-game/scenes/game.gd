f23d5ffcd762d56c10f45c84710be6327a880178
Eric-Lin-24 <zcabel0@ucl.ac.uk>
2025-01-25 16:33:00 +0000

    main3

--- untitled-bubble-game/scenes/game.gd
+++ untitled-bubble-game/scenes/game.gd
@@ -1,15 +1,28 @@
 extends Node2D
 
 @onready var bullet: Area2D = $Bullet
-@onready var timer: Timer = $Timer
+@onready var bullet_timer: Timer = $BulletTimer
+@onready var bubble_timer: Timer = $BubbleTimer
 @onready var cannon: CharacterBody2D = $Cannon
 @onready var marker_2d: Marker2D = %Marker2D
+
+var bubble_x
+var bubble_y
+var random = RandomNumberGenerator.new()
+
+func _on_bubble_time_timeout() -> void:
+	print("your mother")
+	var bubble = preload("res://scenes/bubble.tscn").instantiate()
+	bubble_x = random.randf_range(-525, 525)
+	bubble_y = -700
+	bubble.global_position = Vector2(bubble_x, bubble_y)
+	add_child(bubble)
 
 var canShoot = true
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click") and canShoot:
 		shoot()
-		timer.start(0.3)
+		bullet_timer.start(0.3)
 
 func shoot():
 	canShoot=false
@@ -18,6 +31,5 @@ func shoot():
 	new_bullet.global_position = cannon.global_position
 	add_child(new_bullet)
 
-
 func _on_timer_timeout() -> void:
-	canShoot=true 
+	canShoot=true
